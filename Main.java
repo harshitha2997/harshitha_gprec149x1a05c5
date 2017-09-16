cdddf162fae78d7729e3032b280b3393ff5d48f7
harshitha2997 <31961430+harshitha2997@users.noreply.github.com>
2017-09-16 12:14:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+	public static void main(String[] args) {
+                 
+               Scanner a=new Scanner(System.in);
+               Student students=new Student();
+               int size;
+               System.out.println("enter the no of students:");
+               size=a.nextInt();
+               Student[] b=new Student[size];
+               int id;
+               string fullName;
+               Date birthDate;
+               double avgMark;
+                  
+                 Student s;
+               
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
